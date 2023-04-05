858a2a33de303c6767e9cb021c7c847b52528af3
himanshup1996 <himanshup@valueaddsofttech.com>
2023-04-05 13:44:28 +0530

    first change and update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let http = require('http');
 http.createServer((req,res)=>{
     res.writeHead(200,{'Content-type':'text/html'})
+    res.write("server created........ ")
     res.end("in server");   
 }).listen(8080)
